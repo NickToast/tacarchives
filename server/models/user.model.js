d01c1c063727d61af3067a46e659c5639253341c
Nicholas Tangsouvanh <nicholastangsouvanh@gmail.com>
2023-10-11 20:37:18 -0700

    User login/registration/validation update

---
+++ server/models/user.model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: [true, 'Please enter a first name']
+    },
+    last_name: {
+        type: String,
+        required: [true, 'Please enter a last name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email address'],
+        unique: true
+    },
+    validate: {
+        validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+        message: "Please enter a valid email"
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a valid password'],
+        minlength: [8, 'Password must be at least 6 characters']
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword)
+    .set( value => this._confirmPassword = value);
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this._confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password');
+    }
+    next();
+});
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ require('dotenv').config();
 const port = process.env.PORT;
 const cors = require('cors');
 require('./config/mongoose.config.js')
+const cookieParser = require('cookie-parser');
 
+
+app.use(cookieParser());
 app.use(express.json());
 app.use( express.urlencoded({ extended: true }) );
-app.use(cors())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 // const routeAttacher = require('./routes/pin.routes')
 // routeAttacher(app);

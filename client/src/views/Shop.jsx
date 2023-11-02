bb5ae9b3205382dbc7ec08975303a5e37b3ffce7
Nicholas Tangsouvanh <nicholastangsouvanh@gmail.com>
2023-11-01 20:11:25 -0700

    Shop/listing page update

--- client/src/views/Shop.jsx
+++ client/src/views/Shop.jsx
@@ -29,17 +29,29 @@ const Shop = () => {
     return (
         <div>
             <NavBar/>
-            <h2>Shop</h2>
-            {
-                pinPaths.map((items, key) => {
-                    return (
-                        <>
-                            <h3>{items.image}</h3>
-                            <img src={`http://localhost:3000/` + items.image} alt='pin photo' />
-                        </>
-                    )
-                })
-            }
+            <div className="container">
+                <h2 className='mb-5'>Shop</h2>
+                {/* Left side filter box */}
+                <div className="d-flex">
+                    <div className='col-3 bg-light p-3'>
+                        <h3>Categories</h3>
+                    </div>
+                    {/* Right side listings */}
+                    <div className="d-flex justify-content-evenly col-9 mb-5">
+                    {
+                        pinPaths.map((items, key) => {
+                            return (
+                                <div className='d-flex flex-column gap-1'>
+                                    <img src={`http://localhost:8000/` + items.image} alt='pin photo' className='pin-img mb-4'/>
+                                    <h3>{items.name}</h3>
+                                    <h5>${items.price}</h5>
+                                </div>
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </div>
     )

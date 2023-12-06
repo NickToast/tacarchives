4f44dbbf684897e128e44cf03ca2345d78ebe55c
NickToast <nicholastangsouvanh@gmail.com>
2023-12-06 00:11:18 -0800

    about update

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -16,6 +16,10 @@ const About = () => {
                 <div>
                     {/* hero image */}
                     <img src="" alt="" />
+                    <div>
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, id. Aut, eveniet! Dolores, sequi officiis! Numquam quisquam itaque ducimus corrupti quod? Nesciunt porro ducimus recusandae est, maxime fugit aspernatur ipsa.</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem minima inventore quod reiciendis unde voluptates, voluptate consectetur officia a, earum ex asperiores sunt tempore temporibus soluta corporis ipsam recusandae maxime. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id porro accusantium, temporibus quasi aperiam veritatis, aspernatur debitis molestias nam exercitationem, praesentium quia magnam sit sunt maiores? Aliquam quibusdam illum blanditiis!</p>
+                    </div>
                 </div>
                 {/* maybe parallax? on desktop view */}
                 <div>

d2a95a20998f5ddaaffa8d57c5a6fd3a9713d0b6
NickToast <nicholastangsouvanh@gmail.com>
2023-12-13 23:25:07 -0800

    about updates

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                     </div>
                 </div>
                 {/* maybe parallax? on desktop view */}
-                <div>
+                <div className='parallax'>
 
                 </div>
             </div>

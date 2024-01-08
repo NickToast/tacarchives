81205386ef13500a8539810bfd79a67cd9a4ecc7
NickToast <nicholastangsouvanh@gmail.com>
2024-01-07 23:59:43 -0800

    about update

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -26,6 +26,7 @@ const About = () => {
                 <div className='parallax'>
                     <img src="" alt="" />
                 </div>
+                <div></div>
             </div>
             <Footer/>
         </div>
